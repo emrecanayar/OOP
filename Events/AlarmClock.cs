fcd82ffe3daed3704faa4777acb798d175befc41
emrecanayar <emrecan.ayar@technoface.com.tr>
2024-08-15 15:45:04 +0300

    Events

---
+++ Events/AlarmClock.cs
@@ -0,0 +1,24 @@
+﻿namespace Events
+{
+    // Alarm event'i için oluşturduğumuz delegate.
+    public delegate void AlarmEventHandler(object sender, EventArgs e);
+    public class AlarmClock
+    {
+        // Alarm çaldığında tetiklenecek olan event.
+        public event AlarmEventHandler AlarmRing;
+
+        //Alarmın çalmasını simüle edecek olan metot.
+        public void Ring()
+        {
+            Console.WriteLine("Alarm ringing...");
+            OnAlarmRing(EventArgs.Empty);
+        }
+
+        // Alarm çalındığında event'in tetiklenmesini sağlayan metot.
+        protected virtual void OnAlarmRing(EventArgs e)
+        {
+            // Eğer event'a abone olan metotlar varsa onları tetikle.
+            AlarmRing?.Invoke(this, e);
+        }
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Events;
+
+
+// Alarm çaldığında ekrana mesaj gösteren metot.
+void DisplayMessage(object sender, EventArgs e)
+{
+    Console.WriteLine("Good morning! It's time to wake up!");
+}
+
+// Alarm çaltığında ses çalan metot.
+void PlaySound(object sender, EventArgs e)
+{
+    Console.WriteLine("Playing alarm sound...");
+}
+
+// Alarm çaldığında bir günlük kaydı tutan metot.
+void LogEvent(object sender, EventArgs e)
+{
+    Console.WriteLine("Alarm event logged at: " + DateTime.Now);
+}
+
+// Alarm saatini oluşturuyoruz
+var alarmClock = new AlarmClock();
+
+// Alarm çaldığında yapılacak işlemleri event'e abone ediyoruz
+alarmClock.AlarmRing += DisplayMessage;
+alarmClock.AlarmRing += PlaySound;
+alarmClock.AlarmRing += LogEvent;
+
+// Alarmı çalıyoruz
+alarmClock.Ring();
